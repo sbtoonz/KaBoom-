87212fec223d26065ad3e39b540dc42046be3ea0
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-02-28 19:45:09 -0700

    kaboom

---
+++ Kaboom/ModCore.cs
@@ -0,0 +1,177 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using JetBrains.Annotations;
+using ServerSync;
+using UnityEngine;
+
+namespace Kaboom
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Kaboom : BaseUnityPlugin
+    {
+        private const string ModName = "Kaboom!";
+        private const string ModVersion = "1.0";
+        private const string ModGUID = "Kaboom.Kaboom.Kaboom";
+        private static Harmony harmony = null!;
+
+        internal static GameObject? areaMarker;
+        internal static GameObject? playerMarker;
+        internal static CircleProjector? _projector;
+        ConfigSync configSync = new(ModGUID) 
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion};
+        internal static ConfigEntry<bool> ServerConfigLocked = null!;
+        internal static ConfigEntry<int>? Radius;
+        
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            harmony = new(ModGUID);
+            harmony.PatchAll(assembly);
+
+            Radius = config("General", "Radius of kabooming",
+                10, "How big of a circle to kaboom");
+            ServerConfigLocked = config("General", "Lock Configuration", 
+                true, "If on, the configuration is locked and can be changed by server admins only.");
+
+            configSync.AddLockingConfigEntry(ServerConfigLocked);
+        }
+        
+        
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
+        public static class MarkerGrabber
+        {
+            public static void Postfix(ZNetScene __instance)
+            {
+                if(__instance.m_prefabs.Count <= 0 || __instance.GetPrefab("Wood") ==null) return;
+                areaMarker = Instantiate(__instance.GetPrefab("piece_workbench").transform.Find("AreaMarker").gameObject);
+                var temp = areaMarker.transform.Find("Particle System");
+                DestroyImmediate(temp.gameObject);
+                areaMarker.SetActive(false);
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.OnSpawned))]
+        public static class projecterthign
+        {
+            public static void Postfix(Player __instance)
+            {
+                playerMarker = Instantiate(areaMarker, __instance.transform, false);
+                _projector = playerMarker!.GetComponent<CircleProjector>();
+                playerMarker.SetActive(false);
+            }
+        }
+        
+        [HarmonyPatch(typeof(Player), nameof(Player.RemovePiece))]
+        [HarmonyWrapSafe]
+        public static class KaboomPatch
+        {
+            [UsedImplicitly]
+            public static void Prefix(Player __instance)
+            {
+                if (ZInput.GetButton("AltPlace"))
+                {
+                     Collider[] colliders = Physics.OverlapSphere(Player.m_localPlayer.transform.position,
+                         Radius!.Value, __instance.m_removeRayMask);
+                foreach (var collider in colliders)
+                {
+                 Piece piece = collider.GetComponentInParent<Piece>();
+                        if (piece == null && (bool)collider.GetComponent<Heightmap>())
+                        {
+                            piece = TerrainModifier.FindClosestModifierPieceInRange(collider.transform.position, 2.5f);
+                        }
+                        if ((bool)piece)
+                        {
+                            if (!piece!.m_canBeRemoved)
+                            {
+                                break;
+                            }
+                            if (Location.IsInsideNoBuildLocation(piece.transform.position))
+                            {
+                                __instance.Message(MessageHud.MessageType.Center, "$msg_nobuildzone");
+                                break;
+                            }
+                            if (!PrivateArea.CheckAccess(piece.transform.position))
+                            {
+                                __instance.Message(MessageHud.MessageType.Center, "$msg_privatezone");
+                                break;
+                            }
+                            if (!__instance.CheckCanRemovePiece(piece))
+                            {
+                                break;
+                            }
+                            ZNetView component = piece.GetComponent<ZNetView>();
+                            if (component == null)
+                            {
+                                break;
+                            }
+                            if (!piece.CanBeRemoved())
+                            {
+                                __instance.Message(MessageHud.MessageType.Center, "$msg_cantremovenow");
+                                break;
+                            }
+                            WearNTear component2 = piece.GetComponent<WearNTear>();
+                            if ((bool)component2)
+                            {
+                                component2.Remove();
+                            }
+                            else
+                            {
+                                ZLog.Log("Removing non WNT object with hammer " + piece.name);
+                                component.ClaimOwnership();
+                                piece.DropResources();
+                                piece.m_placeEffect.Create(piece.transform.position, piece.transform.rotation, piece.gameObject.transform);
+                                __instance.m_removeEffects.Create(piece.transform.position, Quaternion.identity);
+                                ZNetScene.instance.Destroy(piece.gameObject);
+                            }
+                            ItemDrop.ItemData rightItem = __instance.GetRightItem();
+                            if (rightItem != null)
+                            {
+                                __instance.FaceLookDirection();
+                                __instance.m_zanim.SetTrigger(rightItem.m_shared.m_attack.m_attackAnimation);
+                            }
+                        }
+                }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.UpdatePlacementGhost))]
+        public static class kaboomcirclepatch
+        {
+            public static void Postfix(Player __instance)
+            {
+                if (ZInput.GetButton("AltPlace"))
+                {
+                    playerMarker!.SetActive(true);
+                    _projector!.m_radius = Radius!.Value;
+                    _projector.m_nrOfSegments = Radius.Value * 4;
+                }
+                else
+                {
+                    playerMarker!.SetActive(false);
+                }
+            }
+        }
+        
+        ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+    }
+}
---
+++ Kaboom/ThunderStore/README.md
@@ -0,0 +1,17 @@
+﻿# KABOOM!
+This mod was requested in the odin+ discord to remove large areas of buildings at once 
+in order to use this mod you hold your alt placement key while removing things you can set your alt placement key in the valheim settings menu (it is default to left shift)
+
+when you are removing things you will see a radius display on the ground around your character
+this is the radius that will be destroyed if you alt place + middle mouse click to remove
+
+middle mouse clicking to remove should work as normal
+
+the radius is configurable and is sync'd to the server if this mod is being used on a server
+
+this mod is server sync'd and is required to live on the server if you wish to use it on one 
+
+good luck have fun
+happy modding
+
+Odin + team
